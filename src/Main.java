39c79a1486ceccd9284469a37fda7450a306ca2d
ArthurLapprand <abl3@cin.ufpe.br>
2017-03-29 20:40:28 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> lines = new ArrayList<String>();
+		
+		for (int i = 0; i < 200; i++) {
+			
+		}
+
+	}
+
+}
